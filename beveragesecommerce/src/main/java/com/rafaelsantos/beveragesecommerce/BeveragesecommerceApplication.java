352765504d93f2e96e4a29fa6ce0c12ab314e5cb
Rafael Santos <rafa.05125@gmail.com>
2024-06-01 18:10:49 -0300

    Project created

---
+++ beveragesecommerce/src/main/java/com/rafaelsantos/beveragesecommerce/BeveragesecommerceApplication.java
@@ -0,0 +1,13 @@
+package com.rafaelsantos.beveragesecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeveragesecommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeveragesecommerceApplication.class, args);
+	}
+
+}
---
+++ beveragesecommerce/src/test/java/com/rafaelsantos/beveragesecommerce/BeveragesecommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafaelsantos.beveragesecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeveragesecommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
