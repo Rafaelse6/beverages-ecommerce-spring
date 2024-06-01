58edc6f0422d4e9415e692ca57660d8c5d892803
Rafael Santos <rafa.05125@gmail.com>
2024-06-01 18:17:01 -0300

    CategoryDTO

--- beveragesecommerce/src/main/java/com/rafaelsantos/beveragesecommerce/entities/DTO/CategoryDTO.java
+++ beveragesecommerce/src/main/java/com/rafaelsantos/beveragesecommerce/entities/DTO/CategoryDTO.java
@@ -1,2 +1,31 @@
-package com.rafaelsantos.beveragesecommerce.entities.DTO;public class CategoryDTO {
+package com.rafaelsantos.beveragesecommerce.entities.DTO;
+
+import com.rafaelsantos.beveragesecommerce.entities.Category;
+
+public class CategoryDTO {
+
+    private Long id;
+    private String name;
+
+    public CategoryDTO() {}
+
+    public CategoryDTO(Long id, String name) {
+        super();
+        this.id = id;
+        this.name = name;
+    }
+
+    public CategoryDTO(Category entity) {
+        id = entity.getId();
+        name = entity.getName();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
 }
