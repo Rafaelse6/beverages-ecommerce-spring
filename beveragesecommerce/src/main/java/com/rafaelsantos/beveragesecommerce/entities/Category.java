bbf8f41be1ee974ced9af21c2122acafb3b1bf45
Rafael Santos <rafa.05125@gmail.com>
2024-06-03 02:29:10 -0300

    Beverage, BeverageDTO

--- beveragesecommerce/src/main/java/com/rafaelsantos/beveragesecommerce/entities/Category.java
+++ beveragesecommerce/src/main/java/com/rafaelsantos/beveragesecommerce/entities/Category.java
@@ -1,13 +1,11 @@
 package com.rafaelsantos.beveragesecommerce.entities;
 
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "tb_category")
@@ -20,6 +18,9 @@ public class Category implements Serializable{
     private Long id;
 
     private String name;
+
+    @ManyToMany(mappedBy = "categories")
+    private Set<Beverage> beverages = new HashSet<>();
 
     public Category() {}
 
@@ -43,6 +44,10 @@ public class Category implements Serializable{
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Set<Beverage> getBeverages(){
+        return beverages;
     }
 
     @Override
--- beveragesecommerce/src/main/resources/import.sql
+++ beveragesecommerce/src/main/resources/import.sql
@@ -1,2 +1,14 @@
 INSERT INTO tb_category(name) VALUES ('Alcoholic')
 INSERT INTO tb_category(name) VALUES ('Non-Alcoholic')
+
+INSERT INTO tb_beverages (name, price, description, img_url) VALUES ('Coca-Cola', 1.99, 'Refreshing soft drink', 'https://example.com/cocacola.jpg');
+INSERT INTO tb_beverages (name, price, description, img_url) VALUES ('Pepsi', 1.89, 'Popular cola beverage', 'https://example.com/pepsi.jpg');
+INSERT INTO tb_beverages (name, price, description, img_url) VALUES ('Orange Juice', 2.99, 'Fresh squeezed orange juice', 'https://example.com/orangejuice.jpg');
+INSERT INTO tb_beverages (name, price, description, img_url) VALUES ('Red Wine', 15.99, 'Dry red wine', 'https://example.com/redwine.jpg');
+INSERT INTO tb_beverages (name, price, description, img_url) VALUES ('Lager Beer', 5.49, 'Crisp and refreshing lager', 'https://example.com/lagerbeer.jpg');
+
+INSERT INTO tb_beverage_category(beverage_id,category_id) VALUES (1,2);
+INSERT INTO tb_beverage_category(beverage_id,category_id) VALUES (2,2);
+INSERT INTO tb_beverage_category(beverage_id,category_id) VALUES (3,2);
+INSERT INTO tb_beverage_category(beverage_id,category_id) VALUES (4,1);
+INSERT INTO tb_beverage_category(beverage_id,category_id) VALUES (5,1);
