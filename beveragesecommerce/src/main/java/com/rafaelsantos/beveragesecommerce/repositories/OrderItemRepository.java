2f6e5ca92410c67de9cb68ea8b8e32cab938828b
Rafael Santos <rafa.05125@gmail.com>
2024-06-03 12:50:33 -0300

    OrderItemRepository

--- beveragesecommerce/src/main/java/com/rafaelsantos/beveragesecommerce/repositories/OrderItemRepository.java
+++ beveragesecommerce/src/main/java/com/rafaelsantos/beveragesecommerce/repositories/OrderItemRepository.java
@@ -1,2 +1,8 @@
-package com.rafaelsantos.beveragesecommerce.repositories;public interface OrderItemRepository {
+package com.rafaelsantos.beveragesecommerce.repositories;
+
+import com.rafaelsantos.beveragesecommerce.entities.OrderItem;
+import com.rafaelsantos.beveragesecommerce.entities.OrderItemPK;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, OrderItemPK> {
 }
