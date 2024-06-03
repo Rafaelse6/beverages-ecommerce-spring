44c546b85514599d0269a1faf1f1c34a55427887
Rafael Santos <rafa.05125@gmail.com>
2024-06-03 03:07:00 -0300

    CategoryController

--- beveragesecommerce/src/main/java/com/rafaelsantos/beveragesecommerce/controllers/CategoryController.java
+++ beveragesecommerce/src/main/java/com/rafaelsantos/beveragesecommerce/controllers/CategoryController.java
@@ -1,2 +1,27 @@
-package com.rafaelsantos.beveragesecommerce.controllers;public class CategoryController {
+package com.rafaelsantos.beveragesecommerce.controllers;
+
+import com.rafaelsantos.beveragesecommerce.entities.DTO.CategoryDTO;
+import com.rafaelsantos.beveragesecommerce.services.CategoryService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryController {
+
+    private CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CategoryDTO>> findAll() {
+        List<CategoryDTO> list = categoryService.findAll();
+        return ResponseEntity.ok(list);
+    }
 }
