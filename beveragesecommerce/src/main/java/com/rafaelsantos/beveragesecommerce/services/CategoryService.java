861fef331c2d91fd29d94846190ab861bdaf8df8
Rafael Santos <rafa.05125@gmail.com>
2024-06-03 03:06:49 -0300

    CategoryService

--- beveragesecommerce/src/main/java/com/rafaelsantos/beveragesecommerce/services/CategoryService.java
+++ beveragesecommerce/src/main/java/com/rafaelsantos/beveragesecommerce/services/CategoryService.java
@@ -1,2 +1,23 @@
-package com.rafaelsantos.beveragesecommerce.services;public class CategoryService {
+package com.rafaelsantos.beveragesecommerce.services;
+
+import com.rafaelsantos.beveragesecommerce.entities.Category;
+import com.rafaelsantos.beveragesecommerce.entities.DTO.CategoryDTO;
+import com.rafaelsantos.beveragesecommerce.repositories.CategoryRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryService {
+
+    private CategoryRepository categoryRepository;
+
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    public List<CategoryDTO> findAll(){
+        List<Category> list = categoryRepository.findAll();
+        return list.stream().map(CategoryDTO::new).toList();
+    }
 }
