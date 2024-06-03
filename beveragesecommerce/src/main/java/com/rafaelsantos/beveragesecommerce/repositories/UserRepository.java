ed4bee19b0fd9ea0b86d6a3a8cfe29bbb925eabb
Rafael Santos <rafa.05125@gmail.com>
2024-06-03 14:47:33 -0300

    UserDetailsProjection, UserDTO

---
+++ beveragesecommerce/src/main/java/com/rafaelsantos/beveragesecommerce/repositories/UserRepository.java
@@ -0,0 +1,2 @@
+package com.rafaelsantos.beveragesecommerce.repositories;public interface UserRepository {
+}
---
+++ beveragesecommerce/src/main/java/com/rafaelsantos/beveragesecommerce/services/exceptions/ForbiddenException.java
@@ -0,0 +1,2 @@
+package com.rafaelsantos.beveragesecommerce.services.exceptions;public class ForbiddenException {
+}
