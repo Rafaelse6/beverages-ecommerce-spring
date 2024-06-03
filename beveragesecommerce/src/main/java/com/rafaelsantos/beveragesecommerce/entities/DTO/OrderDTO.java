b29be90e337c509ab5437e75645db69c5c6fe8d5
Rafael Santos <rafa.05125@gmail.com>
2024-06-03 12:50:04 -0300

    OrderDTO

--- beveragesecommerce/src/main/java/com/rafaelsantos/beveragesecommerce/entities/DTO/OrderDTO.java
+++ beveragesecommerce/src/main/java/com/rafaelsantos/beveragesecommerce/entities/DTO/OrderDTO.java
@@ -1,2 +1,63 @@
-package com.rafaelsantos.beveragesecommerce.entities.DTO;public class OrderDTO {
+package com.rafaelsantos.beveragesecommerce.entities.DTO;
+
+import com.rafaelsantos.beveragesecommerce.entities.Order;
+import com.rafaelsantos.beveragesecommerce.entities.OrderItem;
+import com.rafaelsantos.beveragesecommerce.entities.enums.OrderStatus;
+import jakarta.validation.constraints.NotEmpty;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+public class OrderDTO {
+
+    private Long id;
+    private Instant moment;
+    private OrderStatus status;
+
+    @NotEmpty(message = "Deve ter pelo menos um item")
+    public List<OrderItemDTO> items = new ArrayList<>();
+
+    public OrderDTO(Long id, Instant moment, OrderStatus status) {
+        super();
+        this.id = id;
+        this.moment = moment;
+        this.status = status;
+    }
+
+    public OrderDTO(Order entity) {
+        this.id = entity.getId();
+        this.moment = entity.getMoment();
+        this.status = entity.getStatus();
+
+        for (OrderItem item : entity.getItems()) {
+            OrderItemDTO itemDTO = new OrderItemDTO(item);
+            items.add(itemDTO);
+        }
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public List<OrderItemDTO> getItems() {
+        return items;
+    }
+
+    public Double getTotal() {
+        double sum = 0.0;
+        for (OrderItemDTO item : items) {
+            sum += item.getSubTotal();
+        }
+
+        return sum;
+    }
 }
