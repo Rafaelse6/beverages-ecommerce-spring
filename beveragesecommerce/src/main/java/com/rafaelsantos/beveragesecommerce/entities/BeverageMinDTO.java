bebb2a98a1296680d1605900e146701fc1d92ec1
Rafael Santos <rafa.05125@gmail.com>
2024-06-03 03:05:35 -0300

    BeverageMinDTO

--- beveragesecommerce/src/main/java/com/rafaelsantos/beveragesecommerce/entities/BeverageMinDTO.java
+++ beveragesecommerce/src/main/java/com/rafaelsantos/beveragesecommerce/entities/BeverageMinDTO.java
@@ -1,2 +1,38 @@
-package com.rafaelsantos.beveragesecommerce.entities;public class BeverageMinDTO {
+package com.rafaelsantos.beveragesecommerce.entities;
+
+public class BeverageMinDTO {
+    private Long id;
+    private String name;
+    private Double price;
+    private String imgUrl;
+
+    public BeverageMinDTO(Long id, String name, Double price, String imgUrl) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.imgUrl = imgUrl;
+    }
+
+    public BeverageMinDTO(Beverage entity){
+        id = entity.getId();
+        name = entity.getName();
+        price = entity.getPrice();
+        imgUrl = entity.getImgUrl();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
 }
