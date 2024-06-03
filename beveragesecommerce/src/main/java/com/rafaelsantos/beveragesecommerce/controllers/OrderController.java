e5e432d579e76f7cee853d5beb99c5a3ade762a2
Rafael Santos <rafa.05125@gmail.com>
2024-06-03 14:17:36 -0300

    OrderController

--- beveragesecommerce/src/main/java/com/rafaelsantos/beveragesecommerce/controllers/OrderController.java
+++ beveragesecommerce/src/main/java/com/rafaelsantos/beveragesecommerce/controllers/OrderController.java
@@ -1,2 +1,34 @@
-package com.rafaelsantos.beveragesecommerce.controllers;public class OrderController {
+package com.rafaelsantos.beveragesecommerce.controllers;
+
+import com.rafaelsantos.beveragesecommerce.entities.DTO.OrderDTO;
+import com.rafaelsantos.beveragesecommerce.services.OrderService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping(value = "/orders")
+public class OrderController {
+
+    private OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<OrderDTO> findById(@PathVariable Long id){
+        OrderDTO dto = orderService.findById(id);
+        return ResponseEntity.ok().body(dto);
+    }
+
+    @PostMapping
+    public ResponseEntity<OrderDTO> insert(@Valid @RequestBody OrderDTO dto){
+        dto = orderService.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
+    }
 }
