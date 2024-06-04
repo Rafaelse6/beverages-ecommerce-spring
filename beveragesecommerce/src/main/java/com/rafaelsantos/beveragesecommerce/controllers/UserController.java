a208f61f6456edc5b732afd072625a009f95d186
Rafael Santos <rafa.05125@gmail.com>
2024-06-04 07:53:16 -0300

    UserController

---
+++ beveragesecommerce/src/main/java/com/rafaelsantos/beveragesecommerce/controllers/UserController.java
@@ -0,0 +1,27 @@
+package com.rafaelsantos.beveragesecommerce.controllers;
+
+import com.rafaelsantos.beveragesecommerce.entities.DTO.UserDTO;
+import com.rafaelsantos.beveragesecommerce.services.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserController {
+
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_CLIENT')")
+    @GetMapping(value = "/me")
+    public ResponseEntity<UserDTO> getMe(){
+        UserDTO dto = userService.getMe();
+        return ResponseEntity.ok(dto);
+    }
+}
