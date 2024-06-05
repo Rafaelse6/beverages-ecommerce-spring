69d88e36078316477829ec75fae2b9bcee27f47b
Rafael Santos <rafa.05125@gmail.com>
2024-06-05 01:46:54 -0300

    OrderFactory

--- beveragesecommerce/src/test/java/com/rafaelsantos/beveragesecommerce/factories/OrderFactory.java
+++ beveragesecommerce/src/test/java/com/rafaelsantos/beveragesecommerce/factories/OrderFactory.java
@@ -1,2 +1,19 @@
-package com.rafaelsantos.beveragesecommerce.factories;public class OrderFactory {
+package com.rafaelsantos.beveragesecommerce.factories;
+
+import com.rafaelsantos.beveragesecommerce.entities.*;
+import com.rafaelsantos.beveragesecommerce.entities.enums.OrderStatus;
+
+import java.time.Instant;
+
+public class OrderFactory {
+
+    public static Order createOrder(User client){
+        Order order = new Order(1L, Instant.now(), OrderStatus.WAITING_PAYMENT, client, new Payment());
+
+        Beverage beverage = BeverageFactory.createBeverage();
+        OrderItem orderItem = new OrderItem(order,beverage, 2, 10.0);
+        order.getItems().add(orderItem);
+
+        return order;
+    }
 }
