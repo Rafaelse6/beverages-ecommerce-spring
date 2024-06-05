d7f6dacb7344ec5321585af78f3c3462eec579cc
Rafael Santos <rafa.05125@gmail.com>
2024-06-05 14:10:55 -0300

    REST Assured

---
+++ PocBeveragesEcommerce/src/main/java/com/rafaelsantos/PocBeveragesEcommerce/PocBeveragesEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.rafaelsantos.PocBeveragesEcommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocBeveragesEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocBeveragesEcommerceApplication.class, args);
+	}
+
+}
---
+++ PocBeveragesEcommerce/src/test/java/com/rafaelsantos/PocBeveragesEcommerce/PocBeveragesEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafaelsantos.PocBeveragesEcommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocBeveragesEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
